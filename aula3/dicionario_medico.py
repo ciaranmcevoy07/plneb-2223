863ded3bffe7b625e11865aec72d44fb4b1d4337
Ciaran McEvoy <ciaranmcevoy07@gmail.com>
2023-03-15 23:51:22 +0000

    Atualizacao do dicionario medico

--- aula3/dicionario_medico.py
+++ aula3/dicionario_medico.py
@@ -3,8 +3,8 @@ import re
 file= open('python\dicionario_medico.txt', 'r', encoding="utf8")
 text = file.read()
 text = re.sub(r"\f", "", text)
-text = re.sub(r'(\w+)\n\n(\w+)', r'\1 \2', text)
-entries = re.findall(r'\n\n(.+)((?:\n.+)+)', text)
+new_text = re.sub(r'(\n\n.+)\n\n(.+)', r'\1', text)
+entries = re.findall(r'\n\n(.+)((?:\n.+)+)', new_text)
 new_entries = [(designation, description.strip()) for designation, description in entries]
 print(new_entries[10])
 file.close()
