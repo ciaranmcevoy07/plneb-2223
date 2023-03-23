7258cb09e72a5b434a179f191a699330b727b5b5
Ciaran McEvoy <ciaranmcevoy07@gmail.com>
2023-03-23 11:21:12 +0000

    Update

--- aula4/jsontohtml.py
+++ aula4/jsontohtml.py
@@ -5,16 +5,23 @@ import json
 file = open('novo_dicionario_medico.json', 'r', encoding="utf-8")
 textjson = file.read()
 json_obj = json.loads(textjson)
-html = open('livro_anotado.html', 'r', encoding='utf8')
+
+html = open('small_book.html', 'r', encoding='utf8')
 linhas = html.read().splitlines()
 html_new = open('livro_anotado_new.html', 'w', encoding='utf8')
 for line in linhas:
+    line.strip("!.?,()")
     for key in json_obj:
+        key.strip("!.?,()")
+        print(key)
         a = re.search(r'\b' + key + r'\b', line, re.IGNORECASE)
         if a != None:
             line = line.lower()
-            line = re.sub(r'\b' + key + r'\b', r'<b>' + key + r'</b>', line)
+            expression = "<a data-toggle='tooltip' href='' title='"+ json_obj.get(key) +"'>" + key + "'> </a>"
+            line = re.sub(r'\b' + key + r'\b', expression, line)
     html_new.write(line + "\n")
-html_new.close
+html.close()
+html_new.close()
+file.close()
 
 
--- aula4/teste.py
+++ aula4/teste.py
@@ -1,7 +1,6 @@
 import re
-str='O tst, que ta muito tst'
+str='O Tst, que ta muito tst'
 str2 = 'tst'
 a = re.search(str2 + r'(.+?)', str, re.IGNORECASE)
-new_str = str.lower()
-str = re.sub(r'\b' + str2 + r'\b', 'pneu', new_str)
+str = re.sub(r'\b' + str2 + r'\b', 'pneu', str2, flags=re.IGNORECASE)
 print(str)
