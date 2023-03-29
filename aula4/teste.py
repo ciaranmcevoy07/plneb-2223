af3cacefdc07d5638848d10ece8d8f5d8df7e4a2
Ciaran McEvoy <ciaranmcevoy07@gmail.com>
2023-03-29 15:15:31 +0100

    uh

--- aula4/teste.py
+++
@@ -1,6 +0,0 @@
-import re
-str='O Tst, que ta muito tst'
-str2 = 'tst'
-a = re.search(str2 + r'(.+?)', str, re.IGNORECASE)
-str = re.sub(r'\b' + str2 + r'\b', 'pneu', str2, flags=re.IGNORECASE)
-print(str)
