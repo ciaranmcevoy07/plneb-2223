9944ad491febfba1ddaf81a6c1e862571d79f48f
Ciaran McEvoy <ciaranmcevoy07@gmail.com>
2023-03-30 00:41:38 +0100

    traducao

--- aula5/html_com_anotacoes.py
+++ aula5/html_com_anotacoes.py
@@ -2,15 +2,14 @@ import json, re
 file = open('json_desc_pt_en.json', 'r', encoding="utf-8")
 json_obj = json.load(file)
 
-html = open('livro_anotado.html', 'r', encoding="utf-8")
-linhas = html.read().splitlines()
-html_new = open('livro_anotado_new.html', 'w', encoding='utf8')
+livro = open('LIVRO-Doenças-do-Aparelho-Digestivo.txt', 'r', encoding="utf-8")
+text = livro.read()
 blacklist = {"e", "de", "são"}
 
 nova_lista = json_obj.keys() - blacklist
 nova_lista.remove("L+")
 nova_lista.add("L\+")
-
+'''
 for line in linhas:
     for key in nova_lista:
         exp1 = r"\b" + key + r"\b"
@@ -21,9 +20,40 @@ for line in linhas:
             exp2 = r"\b" + key + r"\b"
             line = re.sub(exp2, expression, line)
     html_new.write(line + "\n")
-html.close()
-html_new.close()
+'''
+expressao = r"\b(" + "|".join(nova_lista) + r")\b"
+
+def anotaTermo(match):
+    """
+    O input da função é automaticamente inserido, mas ele é um objeto do tipo match,
+    no qual o termo de índice 0 é toda a expressão encontrada, o índice 1 o 1º grupo, índice 2 o 2º, etc.
+    """
+    termo=match[1]
+    return f"<a href='' data-toggle='tooltip' title='{json_obj.get(termo)}'>{termo}</a>"
+
+text = re.sub(expressao, anotaTermo, text, flags=re.I)
+text = re.sub(r"\f", "<hr>", text)
+text = re.sub(r"\n", "<br>", text)
+
+header = """
+<html>
+<head>
+<meta charset='uft-8' />
+</head>
+<body>
+"""
+
+body = ""
+body += text
+
+footer = """
+</body>
+</html>
+"""
+livro.close()
+html = open("livro_anotado_traduzido.html", "w", encoding="utf-8")
+html.write(header+body+footer)
 file.close()
-file.close()
+
 
 
