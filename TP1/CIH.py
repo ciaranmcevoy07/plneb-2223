22ec8f6e18517db686ad9b49fc527b6e1226a268
Ciaran McEvoy <ciaranmcevoy07@gmail.com>
2023-04-13 20:24:32 +0100

    dsd

--- TP1/CIH.py
+++ TP1/CIH.py
@@ -1,15 +1,27 @@
 import json
 import re
 
-file = open('CIH Bilingual Medical Glossary English-Spanish.xml', 'r', encoding="utf-8")
-text = file.read()
+file = open('sdsds.xml', 'r', encoding="utf-8")
+text = file.read().splitlines()
+lista1 = []
+lista2 = []
+for i in text:
+    match = re.search(r"<|>",i)
+    if match == None:
+        lista1.append(i)
+for a in lista1:
+    new = a.strip()
+    lista2.append(new)
+print(lista2)
+    
+file.close()
 
-lista1 =re.findall(r"<entry>(\n\s*(.*))*</entry>",text)
 
-for i in lista1:
-    print(i)
-'lista2 = re.findall(r"(?<=<para>\n)([^\n]+)", lista1)'
+
+
+'''lista1 =re.search(r"<entry>(\n\s*(.*))*</entry>",text)
+print(lista1)
+lista2 = re.findall(r"(?<=<para>\n)([^\n]+)", lista1)'''
 
 '''json_CIH = open('json_CIH.json', 'w', encoding="utf-8")
 json_CIH.close()'''
-file.close()
