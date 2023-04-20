515e766a9b360e267668e46a4d5847c53da6bfa5
Ciaran McEvoy <ciaranmcevoy07@gmail.com>
2023-04-20 03:17:21 +0100

    vcvc

--- TP1/AnatomiaGeral/anatomia_geral.py
+++ TP1/AnatomiaGeral/anatomia_geral.py
@@ -4,11 +4,16 @@ import re
 file = open('anatomia_geral.xml', 'r', encoding="utf-8")
 text = file.read()
 file.close()
+'Exclusao de linhas com font="1", font="4", font="9", font="7"'
 text1 = re.sub(r'(.*)(font="1"|font="4"|font="9"|font="7")(.*)', '', text)
+'Selecao de termos em bold ou italico'
 terms = re.findall(r'(?<=<b>)(.*?)(?=</b>)|(?<=<i>)(.*?)(?=</i>)', text1)
 
 clean_list = []
+'Transforma lista de tuplos para lista normal'
 new_list = [item for tup in terms for item in tup]
+
+'Elimina elementos vazios e retira possiveis <b> </b> <i> </i> dos termos guardados na new_list'
 for i in new_list:
     if i != '':
         match = re.search(r'<b>|</b>|<i>|</i>', i)
@@ -16,21 +21,20 @@ for i in new_list:
             clean_list.append(i)
 print(clean_list)
 clean_list2 = []
+
+'Retira espacos a mais e . dos termos'
 for i in clean_list:
     if len(i) != 1:
         a = i.strip()
         b = re.sub(r'\.', '', a)
         clean_list2.append(b)
 
-file4 = open('primeiroteste', 'w', encoding="utf-8")
-for i in clean_list2:
-    file4.write(i + "\n")
-file4.close()
 
 file = open('anatomia_geral.xml', 'r', encoding="utf-8")
 new_file = open('anatomia_geral_editado.xml', 'w', encoding="utf-8")
 new_frases = file.read().splitlines()
 
+'Verifica se os elementos da lista terminam com -, e se sim elimina esse traco e junta os elemento a ser iterado com o proximo'
 elementos_juntos = []
 for i in clean_list2:
         a = re.search(r'^.*-\s*$', i)
@@ -45,6 +49,7 @@ for i in clean_list2:
             if a == None:
                 elementos_juntos.append(i)
 
+'Este loop vai iterar todas as frases to xml e vai filtrar as descricoes como tamben filtrar certos caracteres desnecessarios. Depois cria um xml editado com estas modificacoes.'
 for i in new_frases:
     match1 = re.search(r'(?<=<b>)(.*?)(?=</b>)|(?<=<i>)(.*?)(?=</i>)', i)
     count = new_frases.index(i)
@@ -97,6 +102,7 @@ file2 = open('anatomia_geral_editado.xml', 'r', encoding="utf-8")
 frases = file2.read().splitlines()
 file2.close()
 clean_list3 = []
+'Este loop limpa vai limpar todos os termos de caracteres indesejados'
 for i in elementos_juntos:
         c = re.sub('ﬁ', 'fi', i)
         c = re.sub("ﬂ", "fl", c)
@@ -104,17 +110,13 @@ for i in elementos_juntos:
         clean_list3.append(new_i)
 print(clean_list3)
 
-file4 = open('primeiroteste', 'w', encoding="utf-8")
-for i in clean_list3:
-    file4.write(i + "\n")
-file4.close()
-
 json_file = open('anatomia_geral.json', 'w', encoding="utf-8")
+
+'Por fim este loop vai percorrer todos os termos, pelas frases todas do xml. Se um termo for encontrado no xml, vai procurar as proximas linhas pela definicao. Junta as linhas ate encontrar uma linha com um numero e depois para.'
 string = ''
 dict = {}
 for i in clean_list3:
     for a in frases:
-        'match2 = re.search(r"(<i>|<b>)(\s*)" + i +r"(\s*)(</i>|</b>)", a)'
         match2 = re.search(r'<b>|</b>|<i>|</i>', a)
         if match2:
             if i in a:
@@ -127,12 +129,12 @@ for i in clean_list3:
                     match = re.match(r'[0-9]+', clean_frase_2)
                     if match:
                         print('1')
-                        dict[i] = ''
+                        dict[i] = ""
                     else:
                         n = pos
                         match = re.match(r'[0-9]+', clean_frase_2)
                         secondmatch = re.match(r'</page>', frases[n])
-                        while match is None and secondmatch is None:
+                        while match is None and clean_frase_2 not in string:
                             traco = re.search(r'^.*-\s*$', clean_frase_2)
                             if traco:
                                 new_clean_frase_2 = re.sub(r'-$', ' ', clean_frase_2)
@@ -149,6 +151,7 @@ for i in clean_list3:
                                 if match and secondmatch:
                                     break
                         string = re.sub(r"[^a-zA-Z\s\(\)áàâãçéèêíìîóòôõúùûÁÀÂÄÃÉÈÍÌÎÓÒÔÕÚÙ\-\.]", '', string)
+                        string = re.sub(r'\s*[A-Z]\s+', '', string)
                         dict[i] = string
                         string = ''
 json.dump(dict, json_file, ensure_ascii=False, indent=4)
--- TP1/CIH/CIH.py
+++ TP1/CIH/CIH.py
@@ -7,6 +7,7 @@ file.close()
 file1 = open('CIH_Editado.xml', 'w', encoding="utf-8")
 
 'Remove linhas que nao tem fonte="4 ou 5"'
+text = re.sub(r'(font="1")(?=(.*)(Consciousness))', 'font"4"', text, flags=re.MULTILINE)
 text = re.sub(r'(.*)(font="[0-3|6-9]")(.*)', '', text, flags=re.MULTILINE)
 text = re.sub(r'(.*)(font="10")(.*)', '', text, flags=re.MULTILINE)
 
@@ -16,90 +17,79 @@ text = re.sub(r'^<text\s.*?>(\s)</text>$', '', text, flags=re.MULTILINE)
 'Retira linhas sem nada entre<b> e </b>'
 text = re.sub(r'(.*)<b>\s*</b>(.*)', '', text, flags=re.MULTILINE)
 
-'Retira paragrafos desnecessarios'
+'Retira linhas e paragrafos desnecessarios'
 text = re.sub(r'(.*)(fontspec)(.*)', '', text, flags=re.MULTILINE)
 text = re.sub(r'(.*)(page)(.*)', '', text, flags=re.MULTILINE)
 text = re.sub(r'(.*)(pdf2xml)(.*)', '', text, flags=re.MULTILINE)
 text = re.sub(r'(.*)(xml)(.*)', '', text, flags=re.MULTILINE)
 text = re.sub(r'(.*)(<i>)(.*)', '', text, flags=re.MULTILINE)
+text = re.sub(r'(.*)(:)(.*)', '', text, flags=re.MULTILINE)
+text = re.sub(r'(.*)(<b>)(.*)', '', text, flags=re.MULTILINE)
+text = re.sub(r'(.*)(`)(.*)', '', text, flags=re.MULTILINE)
+text = re.sub(r'(\(TENS\))', '', text, flags=re.MULTILINE)
+text = re.sub(r'(.*)(Constricto/a)(.*)', '', text, flags=re.MULTILINE)
+text = re.sub(r'(.*)(>Pain\s\()(.*)', '', text, flags=re.MULTILINE)  
+text = re.sub(r'(.*)>\)(.*)', '', text, flags=re.MULTILINE)                                                                                                         
+text = re.sub(r'(.*)Vaccine\)(.*)', '', text, flags=re.MULTILINE)
+text = re.sub(r'(.*)>Dolor\s+<(.*)', '', text, flags=re.MULTILINE)
 text = re.sub(r'\n\s*\n', '\n', text)
 file1.write(text)
 file1.close()
+
+'Criar e limpar lista de <b> e espacos adicionais'
 file2 = open('CIH_Editado.xml', 'r', encoding="utf-8")
 text2 = file2.read().splitlines()
 file2.close()
 
 lista = []
-'Limpar lista'
+
 for i in text2:
     match = re.search(r"(.*)", i)
-    if "*" in i:
-        lista.append(i)
-    else:
-        if match:
-            new_i = match.group()
-            new_i = re.sub(r'<b>|</b>', '', new_i)
-            newmatch = re.search(r'(?<=>)(.*)(?=<)', new_i)
-            if newmatch:
-                new_i = newmatch.group()
-                new_i = new_i.strip()
-                lista.append(new_i)
+    if match:
+        new_i = match.group()
+        new_i = re.sub(r'<b>|</b>', '', new_i)
+        newmatch = re.search(r'(?<=>)(.*)(?=<)', new_i)
+        if newmatch:
+            new_i = newmatch.group()
+            new_i = new_i.strip()
+            lista.append(new_i)
+
 'Retira primeiros paragrafos desnecessarios'        
 for i in range(16):
     lista.pop(0)
 json_file = open('CIH.json', 'w', encoding="utf-8")
-
-'Serve para verificar fim de ligacao de palavras com traducao'
-lista.append('')
-
-lista_usados = []
 dict = {}
 
 'Liga as palavras com traducao correta'
-for i in lista:
+k = 0
+while k !=-1:
     string = ''
-    if i in lista_usados or i == ':':
-        print(1)
-    else:
-        pos = lista.index(i)
-        if lista[pos+1] == '':
+    nos = 1
+    count = 2
+    if k+count >= len(lista):
+        break
+    string = lista[k+1]
+    while count != 0:
+        if k+count >= len(lista):
             break
         else:
-            a = lista[pos+1]
-            lista_usados.append(lista[pos+1])
-            if lista[pos+2] == '':
-                break
+            match = re.search(r"^[()[]|^[a-z]|^(Humana)|^(Salud)|^(Paciente)|^(Estreptococcica)|^(Adquirida)|^(Papanicolau)", lista[k+count])
+            match2 = re.search(r'^.*;\s*$', lista[k + count -1])
+            if match or match2:
+                string  = string + lista[k+count]
+                nos = nos +1
+                count = count + 1
             else:
-                match = re.search(r'(^[a-z])|^[\(\)]|^(Humana)|^(Salud)|^(Paciente)|^(Estreptococcica)|^(Adquirida)|^(Papanicolau)|^(Vaccine)', lista[pos+2])
-                if match:
-                    lista_usados.append(lista[pos+2])
-                    if lista[pos+3] == '':
-                        break
-                    else:
-                        match = re.search(r'(^[a-z])|^[\(\)]|^(Humana)|^(Salud)|^(Paciente)|^(Estreptococcica)|^(Adquirida)|^(Papanicolau)|^(Vaccine)', lista[pos+3])
-                        if match:
-                            lista_usados.append(lista[pos+3])
-                            if lista[pos+4] == '':
-                                break
-                            else:
-                                match = re.search(r'(^[a-z])|^[\(\)]|^(Humana)|^(Salud)|^(Paciente)|^(Estreptococcica)|^(Adquirida)|^(Papanicolau)|^(Vaccine)', lista[pos+4])
-                                if match:
-                                    lista_usados.append(lista[pos+3])
-                                else:
-                                    string = string + a + lista[pos+2] + lista[pos+3]
-                                    dict[i] = string
-                        else:
-                            string = string +a + lista[pos+2]
-                            dict[i] = string
+                dict[lista[k]] = string
+                if nos > 1:
+                    k = k + count 
+                    count = 0
                 else:
-                    string = string + a
-                    dict[i] = string        
-
-file4 = open('teste', 'w', encoding="utf-8")
-for i in lista_usados:
-    file4.write(i + "\n")
-file4.close()
-
+                    k = k + 2
+                    count = 0
+v = "Vasectomía"
+dict['Vasectomy'] = v
+    
 json.dump(dict, json_file, ensure_ascii=False, indent=4)
 json_file.close()
 
